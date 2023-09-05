28075bde25069bd9a148c529a6e2a4c7359cae39
Dmitry <sicily007@rambler.ru>
2023-09-05 21:58:37 +0300

    exp051

---
+++ example051/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
+int[,] userArray = GetMatrix();
+PrintMatrix(userArray);
+int sum = GetSum(userArray);
+System.Console.WriteLine($"Сумма элементов главной диагонали = {sum}");
+
+int GetSum(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j) sum += array[i,j];
+        }
+    }
+    return sum;
+}
+
+int[,] GetMatrix(int rows = 4, int columns = 4)
+{
+    Random random = new Random();
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = random.Next(0,100);
+        }
+
+    }
+    return matrix;
+
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
